284a424d342af2bdb6e0a04a0682fc7984dc7cc8
Lucas Santos <91209049+Lvcasfarias@users.noreply.github.com>
2024-04-22 11:16:52 +0000

    Add: Imagens

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <body><h1>Curdo Dev é topzera</h1><br></br><p>Adicionando novas infos</p></body>
+    return <body><h1>Em construção!</h1><br><img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.unirio.br%2Fpropg%2Fdiretoria-de-inovacao-tecnologica-cultural-e-social%2Fimagens%2Fem-construcao%2Fimage_view_fullscreen&psig=AOvVaw0l7YD_WyX9EcWnPYj6cLrp&ust=1713870926955000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIixlPHY1YUDFQAAAAAdAAAAABAE"></img></br></body>
 }
 
 export default Home;
