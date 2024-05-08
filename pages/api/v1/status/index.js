ca002c08f0fcad3f27d93ac255ed5cc4299a46a8
Lucas Santos <91209049+Lvcasfarias@users.noreply.github.com>
2024-05-08 10:46:19 +0000

    add: `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status (require, response) {
+import database from "../../../../infra/database.js"
+
+
+async function status (require, response) {
+  const result = await database.query('Select 1 + 1 as sum;');
+  console.log(result.rows)
   response.status(200).json({ status: 'ok' });
 }
 
